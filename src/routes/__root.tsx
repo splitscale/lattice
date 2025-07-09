43b29b3cca99fa4a5324de86b3c54e7bc14260ed
jeromecabugwason <jeromecabugwason2@protonmail.com>
2025-07-09 23:35:56 +0800

    feat: add header component and integrate navigation menu with Radix UI

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,24 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
+
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
+// If you are a NextJs developer, this is similar to the `app/layout.tsx` file.
 export const Route = createRootRoute({
   component: () => (
-    <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex min-h-screen flex-col bg-background text-foreground">
+        <Header />
+
+        {/* The Outlet component is used to render the next potentially matching child route.
+        <Outlet /> doesn't take any props and can be rendered anywhere within a route's component tree.
+        If there is no matching child route, <Outlet /> will render null. */}
+        <main className="container mx-auto py-6">
+          <Outlet />
+        </main>
+        <TanStackRouterDevtools />
       </div>
-      <hr />
-      <Outlet />
-      <TanStackRouterDevtools />
-    </>
+    </ThemeProvider>
   ),
 });
